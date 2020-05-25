9bd89485681cfaf8862dd83fd3ddeac2c259a2f3
emmeiwhite <emmeiwhite@gmail.com>
2020-05-26 03:12:32 +0530

    Played a lot with arrays and its methods to make sure our todo App is up and running

--- todos-list/script.js
+++ todos-list/script.js
@@ -1,5 +1,6 @@
 const addForm = document.querySelector('.add');
-const lists = document.querySelector('.todos');
+const list = document.querySelector('.todos');
+const search = document.querySelector('.search input');
 
 const generateTemplate = todo => {
   const html = `<li class="list-group-item d-flex justify-content-between align-items-center">
@@ -7,7 +8,7 @@ const generateTemplate = todo => {
     <i class="far fa-trash-alt delete"></i>
   </li>`; // Template string
 
-  lists.innerHTML += html;
+  list.innerHTML += html;
 };
 
 addForm.addEventListener('submit', e => {
@@ -21,8 +22,25 @@ addForm.addEventListener('submit', e => {
 
 // To remove an element we will use event delegation method... Bubbling
 
-lists.addEventListener('click', (e) => {
+list.addEventListener('click', (e) => {
   if (e.target.classList.contains('delete')) {
     e.target.parentElement.remove();
   }
+});
+
+// Search The todo ...
+
+const filterTodos = term => {
+  Array.from(list.children)
+    .filter((todo) => !todo.textContent.toUpperCase().includes(term.toUpperCase()))
+    .forEach(todo => todo.classList.add('filtered'));
+
+  Array.from(list.children)
+    .filter((todo) => todo.textContent.toUpperCase().includes(term.toUpperCase()))
+    .forEach(todo => todo.classList.remove('filtered'));
+};
+// keyup event
+search.addEventListener('keyup', () => {
+  const term = search.value.trim();
+  filterTodos(term);
 });

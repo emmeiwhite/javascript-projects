8836ebe54d92d96ee9802f2147071f42fb2b7974
emmeiwhite <emmeiwhite@gmail.com>
2020-06-12 16:38:11 +0530

    quiz showing results

--- quiz/script.js
+++ quiz/script.js
@@ -1,14 +1,12 @@
 const form = document.querySelector('.questions-form');
+const correctAnswers = ['A', 'B', 'A', 'B', 'B'];
+const result = document.querySelector('.score-wrapper');
 
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
 
   const answersubmitted = [form.q1.value, form.q2.value, form.q3.value, form.q4.value, form.q5.value];
-  const correctAnswers = ['A', 'B', 'A', 'B', 'B'];
-  const scoreHolder = document.querySelector('.score-wrapper');
-  const scoreSpan = document.querySelector('.score-wrapper .score');
-
 
   let score = 0;
   console.log(answersubmitted);
@@ -19,7 +17,8 @@ form.addEventListener('submit', (e) => {
     }
   });
 
-  scoreSpan.textContent = score;
-  scoreHolder.style.display = 'block';
+  result.querySelector('span').textContent = score;
+  // Adding and removing the classes  | Here we are adding the class
+  result.classList.remove('d-none');
 
 });
